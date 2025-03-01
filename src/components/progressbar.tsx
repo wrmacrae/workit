c7dd8564451e1cb2ef06acfa20dae66c205ca619
William Macrae <william.macrae@gmail.com>
2025-03-01 06:52:15 -0500

    progress bar groups sets by exercise visually

--- src/components/progressbar.tsx
+++ src/components/progressbar.tsx
@@ -5,7 +5,7 @@ interface ProgressBarProps {
 }
 
 export const ProgressBar = (props: ProgressBarProps): JSX.Element => {
-    return (<vstack height="100%" alignment="start middle"><vstack height="50%" padding="small" gap="small" cornerRadius='small'>
-        {props.setDonenesses.map((exercise) => exercise.map((set) => set ? <vstack backgroundColor='green' width="20px" grow/>: <vstack backgroundColor='gray' width="20px" grow/>))}
+    return (<vstack height="100%" alignment="start middle"><vstack height="50%" padding="small" gap="medium" cornerRadius='small'>
+        {props.setDonenesses.map((exercise) => <vstack grow gap="small">{exercise.map((set) => set ? <vstack backgroundColor='green' width="20px" grow/>: <vstack backgroundColor='gray' width="20px" grow/>)}</vstack>)}
       </vstack></vstack>)
 }

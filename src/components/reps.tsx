d07f3e925edad97c484f144104e254ef4a385269
William Macrae <william.macrae@gmail.com>
2025-03-18 09:33:52 -0400

    amrap/plank bugfix by keeping timer states in arrays

--- src/components/reps.tsx
+++ src/components/reps.tsx
@@ -4,6 +4,8 @@ import { millisToString } from './timer.js';
 
 interface RepsProps {
     workout: WorkoutData
+    exerciseIndex: number
+    setIndex: number
     set: SetData
     primary: boolean
     increaseReps: () => void
@@ -13,36 +15,39 @@ interface RepsProps {
     setPendingUpdates: StateSetter<never[]>
 }
 
+//TODO Why do I need 2d arrays for time and running? Something was weird with useInterval while running a time and paging around...
 export const Reps = (props: RepsProps): JSX.Element => {
-    const [time, setTime] = useState(0)
-    const [running, setRunning] = useState(false)
-    const interval = useInterval(() => {running ? setTime(time + 1000) : 0}, 1000);
-
+    const [time, setTime] = useState([...Array(10)].map(e => Array(10)))
+    const [running, setRunning] = useState([...Array(10)].map(e => Array(10)))
+    const interval = useInterval(() => {running[props.exerciseIndex][props.setIndex] ? time[props.exerciseIndex][props.setIndex] += 1000 : 0; setTime(time)}, 1000);
     function saveWorkout() {
         props.setWorkout(props.workout);
         props.setPendingUpdates(prev => [...prev, props.workout]);
     }
-
     function toggleTimer() {
-        if (running) {
+        if (running[props.exerciseIndex][props.setIndex]) {
             interval.stop()
-            props.set.time = time
+            props.set.time = time[props.exerciseIndex][props.setIndex]
+            props.set.repsEnteredTime = Date.now()
             saveWorkout()
         } else {
             interval.start()
-            setTime(time + 1)
+            time[props.exerciseIndex][props.setIndex] += 1
+            setTime(time)
         }
-        setRunning(!running)
+        running[props.exerciseIndex][props.setIndex] = !running[props.exerciseIndex][props.setIndex]
+        setRunning(running)
     }
-
     function reset() {
         interval.stop()
-        setRunning(false)
+        running[props.exerciseIndex][props.setIndex] = false
+        setRunning(running)
         delete props.set.time
-        setTime(0)
+        delete props.set.repsEnteredTime
+        time[props.exerciseIndex][props.setIndex] = 0
+        setTime(time)
         saveWorkout()
     }
-
     if (props.set.reps) {
         return (
             <hstack backgroundColor='lightgray' cornerRadius="small" padding="small" width="100px" height="40px">
@@ -54,12 +59,12 @@ export const Reps = (props: RepsProps): JSX.Element => {
             </hstack>
         )
     }
-    if (props.set.time || time) {
+    if (props.set.time || time[props.exerciseIndex][props.setIndex]) {
         return (
             <hstack backgroundColor='lightgray' cornerRadius="small" padding="small" width="100px" height="40px">
-                <vstack cornerRadius="small" borderColor='gray' alignment='center middle' padding="xsmall" onPress={toggleTimer}><icon name={running ? "pause" : "play"} size="xsmall" color="black"/></vstack>
+                <vstack cornerRadius="small" borderColor='gray' alignment='center middle' padding="xsmall" onPress={toggleTimer}><icon name={running[props.exerciseIndex][props.setIndex] ? "pause" : "play"} size="xsmall" color="black"/></vstack>
                 <spacer grow />
-                <vstack onPress={toggleTimer}><text size="large" alignment='center middle' color="black">{running ? millisToString(time) : millisToString(props.set.time)}</text></vstack>
+                <vstack onPress={toggleTimer}><text size="large" alignment='center middle' color="black">{running[props.exerciseIndex][props.setIndex] ? millisToString(time[props.exerciseIndex][props.setIndex]) : millisToString(props.set.time)}</text></vstack>
                 <spacer grow />
                 <vstack cornerRadius="small" borderColor='gray' alignment='center middle' padding="xsmall" onPress={reset}><icon name="delete" size="xsmall" color="black"/></vstack>
             </hstack>

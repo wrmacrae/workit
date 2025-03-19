88378c2a6116a67bb2e31173f3e31b219ba2c6d5
William Macrae <william.macrae@gmail.com>
2025-03-19 06:11:48 -0400

    Revert "amrap/plank bugfix by keeping timer states in arrays"
    This reverts commit d07f3e925edad97c484f144104e254ef4a385269.

--- src/components/reps.tsx
+++ src/components/reps.tsx
@@ -4,8 +4,6 @@ import { millisToString } from './timer.js';
 
 interface RepsProps {
     workout: WorkoutData
-    exerciseIndex: number
-    setIndex: number
     set: SetData
     primary: boolean
     increaseReps: () => void
@@ -15,39 +13,36 @@ interface RepsProps {
     setPendingUpdates: StateSetter<never[]>
 }
 
-//TODO Why do I need 2d arrays for time and running? Something was weird with useInterval while running a time and paging around...
 export const Reps = (props: RepsProps): JSX.Element => {
-    const [time, setTime] = useState([...Array(10)].map(e => Array(10)))
-    const [running, setRunning] = useState([...Array(10)].map(e => Array(10)))
-    const interval = useInterval(() => {running[props.exerciseIndex][props.setIndex] ? time[props.exerciseIndex][props.setIndex] += 1000 : 0; setTime(time)}, 1000);
+    const [time, setTime] = useState(0)
+    const [running, setRunning] = useState(false)
+    const interval = useInterval(() => {running ? setTime(time + 1000) : 0}, 1000);
+
     function saveWorkout() {
         props.setWorkout(props.workout);
         props.setPendingUpdates(prev => [...prev, props.workout]);
     }
+
     function toggleTimer() {
-        if (running[props.exerciseIndex][props.setIndex]) {
+        if (running) {
             interval.stop()
-            props.set.time = time[props.exerciseIndex][props.setIndex]
-            props.set.repsEnteredTime = Date.now()
+            props.set.time = time
             saveWorkout()
         } else {
             interval.start()
-            time[props.exerciseIndex][props.setIndex] += 1
-            setTime(time)
+            setTime(time + 1)
         }
-        running[props.exerciseIndex][props.setIndex] = !running[props.exerciseIndex][props.setIndex]
-        setRunning(running)
+        setRunning(!running)
     }
+
     function reset() {
         interval.stop()
-        running[props.exerciseIndex][props.setIndex] = false
-        setRunning(running)
+        setRunning(false)
         delete props.set.time
-        delete props.set.repsEnteredTime
-        time[props.exerciseIndex][props.setIndex] = 0
-        setTime(time)
+        setTime(0)
         saveWorkout()
     }
+
     if (props.set.reps) {
         return (
             <hstack backgroundColor='lightgray' cornerRadius="small" padding="small" width="100px" height="40px">
@@ -59,12 +54,12 @@ export const Reps = (props: RepsProps): JSX.Element => {
             </hstack>
         )
     }
-    if (props.set.time || time[props.exerciseIndex][props.setIndex]) {
+    if (props.set.time || time) {
         return (
             <hstack backgroundColor='lightgray' cornerRadius="small" padding="small" width="100px" height="40px">
-                <vstack cornerRadius="small" borderColor='gray' alignment='center middle' padding="xsmall" onPress={toggleTimer}><icon name={running[props.exerciseIndex][props.setIndex] ? "pause" : "play"} size="xsmall" color="black"/></vstack>
+                <vstack cornerRadius="small" borderColor='gray' alignment='center middle' padding="xsmall" onPress={toggleTimer}><icon name={running ? "pause" : "play"} size="xsmall" color="black"/></vstack>
                 <spacer grow />
-                <vstack onPress={toggleTimer}><text size="large" alignment='center middle' color="black">{running[props.exerciseIndex][props.setIndex] ? millisToString(time[props.exerciseIndex][props.setIndex]) : millisToString(props.set.time)}</text></vstack>
+                <vstack onPress={toggleTimer}><text size="large" alignment='center middle' color="black">{running ? millisToString(time) : millisToString(props.set.time)}</text></vstack>
                 <spacer grow />
                 <vstack cornerRadius="small" borderColor='gray' alignment='center middle' padding="xsmall" onPress={reset}><icon name="delete" size="xsmall" color="black"/></vstack>
             </hstack>
